79c007f244585fc148c2d8ec89d4f33f4b2c6d59
Jarin Tasnim Diya <0jarintasnimdiya@gmail.com>
2025-08-29 21:17:49 +0600

    App.py

--- App.py
+++ App.py
@@ -16,23 +16,9 @@ from pdfminer3.converter import TextConverter
 import io
 from streamlit_tags import st_tags
 from PIL import Image
-import pymysql
 
 
 
-
-#DATABASE
-
-connection = pymysql.connect(host='localhost',user='root',password='1802Jarin@160',db='resume')
-cursor = connection.cursor()
-
-def insert_data(name,email,res_score,timestamp,no_of_pages,reco_field,cand_level,skills,recommended_skills,courses):
-    DB_table_name = 'user_data'
-    insert_sql = "insert into " + DB_table_name + """
-    values (0,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
-    rec_values = (name, email, str(res_score), timestamp,str(no_of_pages), reco_field, cand_level, skills,recommended_skills,courses)
-    cursor.execute(insert_sql, rec_values)
-    connection.commit()
 
 st.set_page_config(
    page_title="Resume Analyzer",
@@ -43,7 +29,4 @@ def run():
     img = Image.open('static/logo.jpg')
     st.image(img)
     st.title("Resume Analazer")
-    st.sidebar.markdown('#Choose User')
-    activities = ['User','Admin']
 
-    choice = st.sidebar.selectbox("Choose among the given options:", activities)
